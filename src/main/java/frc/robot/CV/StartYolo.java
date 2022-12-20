193d1e45fe7f1d31197218a083cdaadb2f398527
Kaicheng Ouyang <103444532+KaichengOuyang@users.noreply.github.com>
2022-12-20 15:03:53 -0500

    Add files via upload

--- src/main/java/frc/robot/CV/StartYolo.java
+++ src/main/java/frc/robot/CV/StartYolo.java
@@ -3,41 +3,42 @@ package frc.robot.CV;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+
+//TODO: Check if attributes array is working
 
 public class StartYolo {
     public static void main(String[] args) {
         try {
             ProcessBuilder pb = new ProcessBuilder("python", "Yolov7/detect.py", "--source", "1", "--conf-thres", "0.5", "--nosave", "--weights", "Yolov7/yolov7.pt");
             Process process = pb.start();
-            BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            BufferedReader br2 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            BufferedReader sc = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            System.out.println("Initiated main BufferedReader");
+            BufferedReader sc2 = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            System.out.println("Initiated error BufferedReader");
             String line;
-            HashMap<String, Object> itemsDetected = new HashMap<>();
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-                if (!line.contains("|")) {
-                    continue;
-                }
+            ArrayList<Item> itemsDetected = new ArrayList<>();
+            System.out.println("Initiated ArrayList");
+            while ((line = sc.readLine()) != null) {
+                    System.out.println(line);
                     String[] objects = line.split("\\|");
-                    for (String s : objects) {
-                        String[] attributes = s.split(" ");//Split the string into an array of attributes(name, center, BBOXP1, BBOXP2, confidence)
-                        String PLCCoord = attributes[1].substring(1, attributes[1].length() - 1);//Get the center coordinates
-                        String[] center = PLCCoord.split(",");//Split the center coordinates into an array of x and y
-                        String BBOXP1Coord = attributes[2].substring(1, attributes[2].length() - 1);//Get the BBOXP1 coordinates
-                        String[] BBOXP1 = BBOXP1Coord.split(",");//Split the BBOXP1 coordinates into an array of x and y
-                        String BBOXP2Coord = attributes[3].substring(1, attributes[3].length() - 1);//Get the BBOXP2 coordinates
-                        String[] BBOXP2 = BBOXP2Coord.split(",");//Split the BBOXP2 coordinates into an array of x and y
-                        //double Confidence = Double.parseDouble(attributes[4]);//Confidence
-                        itemsDetected.put(attributes[0], new Object(attributes[0], new int[]{Integer.parseInt(center[0]), Integer.parseInt(center[1])}, new int[]{Integer.parseInt(BBOXP1[0]), Integer.parseInt(BBOXP1[1])}, new int[]{Integer.parseInt(BBOXP2[0]), Integer.parseInt(BBOXP2[1])}, attributes[4]));
+                    try {
+                        for (String s : objects) {
+                            String[] attributes = s.split(" ");//Split the string into an array of attributes(name, center, BBOXP1, BBOXP2, confidence)
+                            String[] center = attributes[1].split(",");//Split the center into an array of x and y
+                            String[] BBOXP1 = attributes[2].split(",");//Split the BBOXP1 into an array of x and y
+                            String[] BBOXP2 = attributes[3].split(",");//Split the BBOXP2 into an array of x and y
+                            Item item = new Item(attributes[0], new int[]{Integer.parseInt(center[0]), Integer.parseInt(center[1])}, new int[]{Integer.parseInt(BBOXP1[0]), Integer.parseInt(BBOXP1[1])}, new int[]{Integer.parseInt(BBOXP2[0]), Integer.parseInt(BBOXP2[1])}, attributes[4]);
+                            itemsDetected.add(item);
+                            sc.close();
+                            System.out.println(item.getName());
+                        }
+                        for (Item object : itemsDetected) {
+                            System.out.println(object.getName());
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Unable to parse line");
                     }
-                    for (String s : itemsDetected.keySet()) {
-                        System.out.println(itemsDetected.get(s).toString());
-                    }
-                while ((line = br2.readLine()) != null) {
-                    System.out.println("Error " + line);
                 }
-            }
         } catch (Exception e) {
             e.printStackTrace();
         }
